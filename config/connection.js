367276e06be192d8d4d4945aa70d0c9aa3d7ddb8
Kevin Duong <duong.kev@gmail.com>
2017-01-14 14:28:03 -0800

    Updated 1.14.17

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "parties_db"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
