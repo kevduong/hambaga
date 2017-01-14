909d341bcd2300b3fff6f1b59cb651d423386a1c
Kevin Duong <duong.kev@gmail.com>
2017-01-14 11:50:05 -0800

    Boilerplates added --needs to be updated

--- server.js
+++ server.js
@@ -1,50 +1,17 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+var orm = require("./config/orm.js");
 
-var app = express();
-var port = 3000;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+// Console log all the party_name's.
+orm.select("*", "parties");
 
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// Console log all the client_name's.
+orm.select("client_name", "clients");
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+// Console log all the parties that have a party-type of grown-up.
+orm.selectWhere("parties", "party_type", "grown-up");
 
-var exphbs = require("express-handlebars");
+// Console log all the clients and their parties.
+orm.leftJoin("*", "clients", "parties", "id", "client_id");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "day_planner_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-
-});
-
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM plans;", function(err, data) {
-    if (err) {
-      throw err;
-    }
-
-    res.render("index", { plans: data });
-
-  });
-});
+// Console log all the clients and their parties.
+orm.rightJoin("*", "clients", "parties", "id", "client_id");
