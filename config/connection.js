c15aac5e176d1a03be1e7f3e7bbff50fe4c1602d
Kevin Duong <duong.kev@gmail.com>
2017-01-21 16:12:06 -0800

    fixing sql

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
  connection = mysql.createConnection({
-  host: "localhost",
+  host: "local",
   user: "root",
   password: "password",
   database: "burgers_db"
