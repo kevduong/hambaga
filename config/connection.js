363759f7af10a2bf192d13438a366e699c8fb204
Kevin Duong <duong.kev@gmail.com>
2017-01-21 16:56:34 -0800

    added port to connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
+// var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
  connection = mysql.createConnection({
   port: 3306,
-  host: "local",
+  host: "localhost",
   user: "root",
   password: "password",
   database: "burgers_db"
